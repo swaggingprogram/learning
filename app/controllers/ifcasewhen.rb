aa1d00f057ceb1253aa97a64dd0c7e78069d6c53
swaggingprogram <ogura0908work@gmail.com>
2022-01-06 16:47:09 +0900

    ifcasewhen

---
+++ app/controllers/ifcasewhen.rb
@@ -0,0 +1,29 @@
+unless status == "ok"
+  "aaaa"
+end
+
+変数が偽の場合に真となる
+
+unless status == "ok"
+  "aaaa"
+else
+  "bbbb"
+end
+
+変数が真の場合はelseが適用される。
+
+-----------------------------------------------
+
+case オブジェクト
+when 条件1
+  puts "aaaa"
+when 条件2
+  puts "aaaa"
+else 条件3
+  puts "aaaaaa"
+end
+
+if elsifを全部whenにできるから便利
+
+
+
