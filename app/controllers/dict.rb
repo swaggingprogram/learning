a314b8673e5d2cfd0887863e4a77839f03b9e87f
swaggingprogram <ogura0908work@gmail.com>
2022-01-07 14:57:57 +0900

    test

--- app/controllers/dict.rb
+++ app/controllers/dict.rb
@@ -13,6 +13,8 @@
 
 .reverse :反転させる
 
+.capitalize :最初の1文字を大文字にするメソッド
+
 !メソッドはメソッド外の変数の数値も変えるため、注意が必要
 
 s = "ruby"
@@ -24,3 +26,32 @@ end
 s = "YBUR"となるので注意！
 
 pp putsで改行されないものを改行して出す
+
+require 'minitest/autorun'
+
+class SampleTest < Minitest::Test
+  def test_sample
+    assert_equal "RUBY", "ruby".upcase
+  end
+end
+
+require 'minitest/autorun'
+minitestを呼び出す宣言
+
+class SampleTest < Minitest::Test
+end
+Minitest::Testクラスから継承したクラスであることを宣言
+
+def test_sample
+  assert_equal "RUBY", "ruby".upcase
+end
+test_sample minitestはtest_を含むメソッドを調べている
+
+assert_equalメソッド (assert_equal, b, a)
+aの実行結果がbとなることを調べるメソッド
+
+assert a 
+aが真であることを調べるメソッド
+
+refuse a
+aが偽であることを調べるメソッド
