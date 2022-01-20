e1a3cb53c47b2b7f9e8992770512eaf2e9c06f72
hideofhide <92572199+swaggingprogram@users.noreply.github.com>
2022-01-20 19:19:16 +0900

    method

--- app/controllers/rgb.rb
+++ app/controllers/rgb.rb
@@ -1,7 +1,14 @@
 def to_hex(r, g, b)
-    "#" +
-    r.to_s(16).rjust(2,"0") +
-    g.to_s(16).rjust(2,"0") +
-    b.to_s(16).rjust(2,"0") +
+  "#" +
+  r.to_s(16).rjust(2,"0") +
+  g.to_s(16).rjust(2,"0") +
+  b.to_s(16).rjust(2,"0") +
 end
 
+def to_hex(r,g,b)
+  hex = "#"
+  [r,g,b].each do |n|
+      n.to_s(16).rjust(2, "0")
+  end
+end
+    
