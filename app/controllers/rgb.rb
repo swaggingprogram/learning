477b90501e36dc9bf90f9c5de899467e0d02d129
hideofhide <92572199+swaggingprogram@users.noreply.github.com>
2022-01-21 18:04:45 +0900

    to_ints

--- app/controllers/rgb.rb
+++ app/controllers/rgb.rb
@@ -10,5 +10,12 @@ def to_hex(r,g,b)
   [r,g,b].each do |n|
       n.to_s(16).rjust(2, "0")
   end
+  hex
+end
+
+def to_hex(r,g,b)
+  [r,g,b].sum("#") do |n|
+    n.to_s(16).rjust(2, "0")
+  end
 end
     
--- app/controllers/rgb_test.rb
+++ app/controllers/rgb_test.rb
@@ -6,4 +6,7 @@ class RgbTest < Minitest::Test
       assert_equal '#000000', to_hex(0,0,0)asser
       assert_equal '#ffffff', to_hex(255, 255, 255)
   end
+
+  def test_to_ints
+    assert_equal [0,0,0], to_ints("#000000")
 end
