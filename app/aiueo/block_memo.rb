dc59dc74b391d980eaf82f87f71df7e50d011f80
hideofhide <92572199+swaggingprogram@users.noreply.github.com>
2022-01-25 16:38:16 +0900

    block

--- app/aiueo/block_memo.rb
+++ app/aiueo/block_memo.rb
@@ -26,3 +26,41 @@ fruits.delete_if.with_index {|fruit, i| fruit.include?("a") && i.odd?}
 ↑配列にaが含まれかつ添え字が奇数との時のみ、削除される
 
 = ["apple", "melon"]
+
+with_indexメソッドに引数を与えると、0からじゃなくその数値からスタートする
+fruits = ["apple","orange","melon"]
+fruits.map.with_index(1) {|fruit, i| "#{i}: #{fruit}"}
+= [1: "apple", 2: "orange", 3: "melon"]
+
+dimensions = [
+  [10, 20],
+  [30, 40],
+  [50, 60]
+]
+
+dimensions.each_with_index do |length, width, i|
+  puts "length: #{lengt}, width: #{width}, i:#{i}"
+end
+=
+length:[10,20], width: 0, i:nil
+length:[30, 40], width: 1, i:nil
+length:[50, 60],  width: 2, i:nil
+
+dimensions.each_with_index do |dimension, i|
+  length = dimension[0]
+  width = dimension[1]
+  puts "length: #{lengt}, width: #{width}, i:#{i}"
+end
+
+dimensions = [
+  [10, 20],
+  [30, 40],
+  [50, 60]
+]
+
+dimensions.each_with_index do |(length, width), i|
+  puts "length: #{lengt}, width: #{width}, i:#{i}"
+end
+
+ブロックパラメーターを()で囲うことで、パラメーターの別々の要素として扱える
+
