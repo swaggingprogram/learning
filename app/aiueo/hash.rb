9f3061613481d7625adc9fc09514c9e6ace29246
hideofhide <92572199+swaggingprogram@users.noreply.github.com>
2022-01-27 18:23:25 +0900

    hash2

--- app/aiueo/hash.rb
+++ app/aiueo/hash.rb
@@ -7,3 +7,19 @@ currencies["japan"] = "円":ハッシュの上書き
 
 ハッシュの値を呼び出す：currencies["us"] = "dollar"
 
+currencies = {"japan" => "yen", "us" => "dollar", "india" => "rupee"}
+currencies.each do |key, value|
+  puts "#{key},#{value}"
+end
+
+ハッシュに繰り返し処理をかけるときは、キーとバリュー用の変数が必要
+
+
+パラメーターを１つにすると、キーと値が配列になってしまう
+currencies = {"japan" => "yen", "us" => "dollar", "india" => "rupee"}
+currencies.each do |key_value|
+  key = key_value[0]
+  value = key_value[1]
+  puts "#{key},#{value}"
+end
+#"japan" => "yen" 0と１に格納される
