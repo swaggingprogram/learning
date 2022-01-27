6c20671513a1802de68cabc15aeb086c85fa6128
hideofhide <92572199+swaggingprogram@users.noreply.github.com>
2022-01-27 18:17:09 +0900

    hash1

---
+++ app/aiueo/hash.rb
@@ -0,0 +1,9 @@
+hash: {}
+
+currencies = {"japan" => "yen", "us" => "dollar", "india" => "rupee"}
+currencies["italy"] = "euro" :ハッシュにキーと要素を追加する
+
+currencies["japan"] = "円":ハッシュの上書き
+
+ハッシュの値を呼び出す：currencies["us"] = "dollar"
+
